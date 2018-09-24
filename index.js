dcffcc9e5df7ec437bb12c191d064dfd821d3503
Borko <rajkovicborko@gmail.com>
2018-09-24 19:19:26 +0200

    on disconnect, mark socket id as disconnected and emit

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import startServer from './src/server';
 export const store = makeStore();
 startServer(store);
 
-//TODO on disconnect, mark socket id as disconnected and emit
 //TODO on socket emit change name, change it's name and emit
 //TODO on socket emit typing, update typing for that receiver and emit
 //TODO on socket emit ENTER, update finished message for that receiver and emit
