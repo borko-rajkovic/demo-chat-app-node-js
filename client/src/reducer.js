90e1b5282ba8de1b358a6f49cec84f52b83b880c
Borko Rajkovic <rajkovicborko@gmail.com>
2018-09-24 16:03:23 +0200

    Tested reducer

--- client/src/reducer.js
+++ client/src/reducer.js
@@ -1,35 +1,56 @@
-import { List, Map } from 'immutable';
+import _ from 'lodash';
 
-function setState(state, newState) {
-    return state.merge(newState);
-}
+import Reducer from './reducer-template';
 
-function vote(state, entry) {
-    const currentPair = state.getIn(['vote', 'pair']);
-    if (currentPair && currentPair.includes(entry)) {
-        return state.set('hasVoted', entry);
-    } else {
-        return state;
+class ArticlesReducer extends Reducer {
+
+    getInitialState() {
+        return {
+            retrieved: false,
+            loading: true,
+            errored: false,
+            topics: []
+        };
+    }
+
+    getTypeHandlers() {
+        return {
+            'GET_ARTICLES_FULFILLED': this.onArticlesRetrieved,
+            'GET_ARTICLES_REJECTED': this.onArticlesRejected,
+            'INIT_ARTICLES': this.onInitArticles
+        };
+    }
+
+    onArticlesRetrieved(state, payload) {
+        return _.extend({}, state, {
+            retrieved: true,
+            loading: false,
+            errored: false,
+            topics: payload.data
+        });
+    }
+
+    onArticlesRejected(state) {
+        return _.extend({}, state, {
+            retrieved: true,
+            loading: false,
+            errored: true
+        });
+    }
+
+    onInitArticles(state) {
+        let topics = null;
+
+        if(topics) {
+            topics = JSON.parse(topics);
+        }
+
+        return _.extend({}, state, {
+            retrieved: !!topics,
+            loading: false,
+            topics: topics
+        });
     }
 }
 
-function resetVote(state) {
-    const hasVoted = state.get('hasVoted');
-    const currentPair = state.getIn(['vote', 'pair'], List());
-    if (hasVoted && !currentPair.includes(hasVoted)) {
-        return state.remove('hasVoted');
-    } else {
-        return state;
-    }
-}
-
-export default function (state = Map(), action) {
-    switch (action.type) {
-        case 'SET_STATE':
-            return resetVote(setState(state, action.state));
-        case 'VOTE':
-            return vote(state, action.entry);
-        default:
-          return state;
-    }
-}
+export default ArticlesReducer.getInstance();
